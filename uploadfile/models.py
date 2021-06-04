a94eeada44a5d9f4b1a13348d7aa9d61cae71abd
Muhammad Osama <you@example.com>
2021-06-04 14:33:45 +0500

    Third Commit

--- uploadfile/models.py
+++ uploadfile/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
+
 # Create your models here.
+class Customer(models.Model):
+    status_choice = (
+        ('Delivered', 'Delivered'), ('Pending', 'Pending'), ('Cancel', 'Cancel')
+    )
+    pickup_address_id = models.IntegerField(null=True, blank=True)
+    show_information_on_air_waybill = models.CharField(max_length=50, null=True, blank=True)
+    consignee_city_name = models.CharField(max_length=100, null=True, blank=True)
+    consignee_name = models.CharField(max_length=150, null=True, blank=True)
+    consignee_address = models.CharField(max_length=225, null=True, blank=True)
+    consignee_phone_number_1 = models.CharField(max_length=150, null=True, blank=True)
+    consignee_phone_number_2 = models.CharField(max_length=150, null=True, blank=True)
+    consignee_email_address = models.EmailField(max_length=150, null=True, blank=True)
+    self_collection = models.CharField(max_length=50, null=True, blank=True)
+    order_id = models.CharField(max_length=50, null=True, blank=True)
+    order_date = models.DateField(null=True, blank=True)
+    item_product_type_id = models.CharField(max_length=50, null=True, blank=True)
+    item_description = models.CharField(max_length=225, null=True, blank=True)
+    item_quantity = models.CharField(max_length=100, null=True, blank=True)
+    item_insurance = models.CharField(max_length=50, null=True, blank=True)
+    product_value = models.IntegerField(null=True, blank=True)
+    special_instructions = models.CharField(max_length=225, null=True, blank=True)
+    estimated_weight = models.IntegerField(null=True, blank=True)
+    mode_of_shipment_id = models.CharField(max_length=150, null=True, blank=True)
+    same_day_timing_id = models.CharField(max_length=150, null=True, blank=True)
+    collection_amount = models.IntegerField(null=True, blank=True)
+    mode_of_payment_id = models.IntegerField(null=True, blank=True)
+    charges_mode_id = models.IntegerField(null=True, blank=True)
+    pieces = models.IntegerField(null=True, blank=True)
+    shipper_reference_number_1 = models.CharField(max_length=150, null=True, blank=True)
+    shipper_reference_number_2 = models.CharField(max_length=150, null=True, blank=True)
+    shipper_reference_number_3 = models.CharField(max_length=150, null=True, blank=True)
+    shipper_reference_number_4 = models.CharField(max_length=150, null=True, blank=True)
+    shipper_reference_number_5 = models.CharField(max_length=150, null=True, blank=True)
+    Status = models.CharField(max_length=150, choices=status_choice)
+
+    def __str__(self):
+        return self.consignee_name
+
+    class Meta:
+        verbose_name = 'Customer'
+        verbose_name_plural = 'Customer'
--- uploadfile/views.py
+++ uploadfile/views.py
@@ -1,10 +1,13 @@
+import datetime
 import pandas as pd
-from django.http import JsonResponse
-
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Customer
 
 # Create your views here.
+from uploadfile.models import Customer
+
+
 def home(request):
     return render(request, "upload.html")
 
@@ -13,4 +16,64 @@ def file(request):
     file1 = request.FILES.get('upload')
     read_file = pd.read_csv(file1)
     json_data = read_file.to_json()
-    return JsonResponse({'json_data': json_data})
+    for row in read_file.to_dict(orient='records'):
+        Customer.objects.update_or_create(consignee_name=row.get("Consignee Name"),
+
+                                          pickup_address_id=row.get("Pickup Address ID"
+                                                                    ),
+                                          show_information_on_air_waybill=row.get("Show Information on Air Waybill"
+                                                                                  ),
+                                          consignee_city_name=row.get("Consignee City Name"
+                                                                      ),
+                                          consignee_address=row.get("Consignee Address"
+                                                                    ),
+                                          consignee_phone_number_1=row.get("Consignee Phone Number 1 (03000000000)"
+                                                                           ),
+                                          consignee_phone_number_2=row.get("Consignee Phone Number 2 (03000000000)"
+                                                                           ),
+                                          consignee_email_address=row.get("Consignee Email Address"
+                                                                          ),
+                                          self_collection=row.get("Self Collection"),
+
+                                          order_id=row.get("Order ID"
+                                                           ),
+                                          order_date=datetime.datetime.strptime(
+                                              row.get("Order Date (YYYY-MM-DD)"), '%d/%m/%Y'),
+                                          item_product_type_id=row.get("Item Product Type ID"
+                                                                       ),
+                                          item_description=row.get("Item Description"
+                                                                   ),
+                                          item_quantity=str(row.get("Item Quantity"
+                                                                    )),
+                                          item_insurance=row.get("Item Insurance"
+                                                                 ),
+                                          product_value=row.get("Product Value"
+                                                                ),
+                                          special_instructions=row.get("Special Instructions"
+                                                                       ),
+                                          estimated_weight=row.get("Estimated Weight (kg)"
+                                                                   ),
+                                          mode_of_shipment_id=row.get("Mode of Shipment ID"
+                                                                      ),
+                                          same_day_timing_id=row.get("Same Day Timing ID"
+                                                                     ),
+                                          collection_amount=row.get("Collection Amount"
+                                                                    ),
+                                          mode_of_payment_id=row.get("Mode of Payment ID"
+                                                                     ),
+                                          charges_mode_id=row.get("Charges Mode ID"
+                                                                  ),
+                                          pieces=row.get("Pieces"
+                                                         ),
+                                          shipper_reference_number_1=row.get("Shipper Reference Number 1"
+                                                                             ),
+                                          shipper_reference_number_2=row.get("Shipper Reference Number 2"
+                                                                             ),
+                                          shipper_reference_number_3=row.get("Shipper Reference Number 3"
+                                                                             ),
+                                          shipper_reference_number_4=row.get("Shipper Reference Number 4"
+                                                                             ),
+                                          shipper_reference_number_5=row.get("Shipper Reference Number 5"
+                                                                             ))
+
+    return HttpResponse()
